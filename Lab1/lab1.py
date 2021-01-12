b66a5d7639739a4d22cf9875fe57409471921f21
Hongru Qi <35446863+MichaelQi11@users.noreply.github.com>
2021-01-11 19:31:19 -0700

    Update lab1.py

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -4,3 +4,6 @@ print(re.__version__)
 
 google_resp = re.get('https://www.google.com')
 print(google_resp)
+
+code_resp = re.get('https://raw.github.com/MichaelQi11/CMPUT404/master/Lab1/lab1.py')
+print(code_resp.text)
